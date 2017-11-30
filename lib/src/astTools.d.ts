f6c42da5331f1aecd10b51aa25a25daec381fc28
Chi Vinh Le <vinh@wikiwi.io>
2017-11-30 21:15:15 +0100

    Include compiled lib

---
+++ lib/src/astTools.d.ts
@@ -0,0 +1,3 @@
+export declare function mergeDefinitions(a: any, b: any): any;
+export declare function mergeSelectionSets(a: any, b: any): any;
+export declare function resolveNamedFragmentsAndDirectives(document: any, options?: any): any;
